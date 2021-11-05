ec114b54c1554c26e64a5204ea72dde82c1b84b2
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2021-11-05 12:01:08 -0300

    1a lição do tutorial
    Primeira lição do tutorial com os pontos chaves na forma de comentário. O programa não faz nada;

---
+++ C introduction/C keywords and identifiers.c
@@ -0,0 +1,52 @@
+/*
+
+Caracteres aceitos em C:
+Alfábéticos:
+Uppercase: A B C ................................... X Y Z
+Lowercase: a b c ...................................... x y z
+
+Números:
+0 1 2 3 4 5 6 7 8 9
+
+Caracteres especiais:
+
+,  <  >  .  _  (  )  ;  $  :  %  [  ]  #  ?  '  &  {  }
+
+"  ^  !  *  /  |  -  \  ~  +
+
+
+Caracteres que aparecem em branco:
+
+' '(espaço), '\n'(quebra-linha), '\t'(tab horizontal), '\r'(carrige return), '\f'(form feed)
+
+
+Palavras-chave em C:
+auto,	double,	int,	struct
+break,	else,	long,	switch
+case,	enum,	register,	typedef
+char,	extern,	return,	union
+continue,	for,	signed,	void
+do,	if,	static,	while
+default,	goto,	sizeof,	volatile
+const,	float,	short,	unsigned
+
+
+Regras para criar identificadores (variáveis):
+1. Podem ser usados tanto letras maiúsculas como minúsculas e números, além de
+underlines ou underscores.
+2. O primeiro caracter do identificador deve ser um underline ou letra.
+3. Não podem ser usadas palavras chave de C.
+4. Não existe tamanho limite para identificadores, mas caso ele seja maior que
+31 caracteres, pode dar problemas em alguns compiladores.
+
+*/
+
+
+
+#include <stdio.h>
+
+
+int main() {
+
+  return 0;
+}
